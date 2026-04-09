5e884aa821d345200b7c7e0d03eb57cd91666925
joaodavim1 <joaodavim1@users.noreply.github.com>
2026-04-08 23:30:20 -0300

    Use dark styling for menu and multi launch

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "financeiro-pwa-main-cache-";
-const CACHE_NAME = `${CACHE_PREFIX}v77`;
+const CACHE_NAME = `${CACHE_PREFIX}v78`;
 const APP_SHELL = [
   "./",
   "./index.html",
