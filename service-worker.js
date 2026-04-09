046d3b0f148c7b8a799155bf1f1a19b84b4aee9f
joaodavim1 <joaodavim1@users.noreply.github.com>
2026-04-09 18:40:10 -0300

    Fix theme contrast and light menu styling

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "financeiro-pwa-main-cache-";
-const CACHE_NAME = `${CACHE_PREFIX}v89`;
+const CACHE_NAME = `${CACHE_PREFIX}v90`;
 const APP_SHELL = [
   "./",
   "./index.html",
