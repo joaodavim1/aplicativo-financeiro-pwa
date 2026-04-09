36492a991dc1b3ec30d83ab600ff5025c7b1e14a
joaodavim1 <joaodavim1@users.noreply.github.com>
2026-04-09 19:22:37 -0300

    Resize mobile nav buttons

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "financeiro-pwa-main-cache-";
-const CACHE_NAME = `${CACHE_PREFIX}v92`;
+const CACHE_NAME = `${CACHE_PREFIX}v93`;
 const APP_SHELL = [
   "./",
   "./index.html",
