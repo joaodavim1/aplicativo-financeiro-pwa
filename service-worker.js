120d16eafb42141831514822046a8025734aaf21
joaodavim1 <joaodavim1@users.noreply.github.com>
2026-04-08 23:34:21 -0300

    Improve dark theme contrast

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "financeiro-pwa-main-cache-";
-const CACHE_NAME = `${CACHE_PREFIX}v78`;
+const CACHE_NAME = `${CACHE_PREFIX}v79`;
 const APP_SHELL = [
   "./",
   "./index.html",
