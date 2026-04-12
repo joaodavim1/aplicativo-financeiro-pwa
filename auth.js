252d9f95cc04c9a1d52c7d1f46bc7138d994158b
joaodavim1 <joaodavim1@users.noreply.github.com>
2026-04-11 22:55:21 -0300

    Improve dark contrast on futuro screen

--- auth.js
+++ auth.js
@@ -1,8 +1,8 @@
-import { bootFinanceiroApp, getFinanceiroMenuState } from "./app.js?v=20260411ak";
+import { bootFinanceiroApp, getFinanceiroMenuState } from "./app.js?v=20260411al";
 
 const IOS_APP_VERSION = "Versão atual: 1.14";
 const IOS_SETTINGS_VERSION = "1.14";
-const IOS_BUILD_TOKEN = "20260411ak";
+const IOS_BUILD_TOKEN = "20260411al";
 const BUILD_STORAGE_KEY = "financeiro-pwa-build-token";
 
 const runtimeConfig = window.FINANCEIRO_SUPABASE_CONFIG || null;
